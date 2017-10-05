a62a40887504fd0a8e8c775433c375f63159d2c9
Gunther Schmidl <gunther.schmidl@avocodo.com>
2017-10-05 10:27:02 +0200

    allow command-line parameter to autostart file

--- CitrixReceiverIcaFileManipulator/Program.cs
+++ CitrixReceiverIcaFileManipulator/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -12,11 +9,11 @@ namespace WindowsFormsApp1
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new MainForm(args != null && args.Length > 0 ? args[0] : null));
         }
     }
 }
